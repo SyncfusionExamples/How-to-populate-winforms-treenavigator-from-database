801086a1f42d04ec05f322874709fcbf50cf8f15
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-07-10 17:43:08 +0530

    Added sample for populate the tree navigator to database

---
+++ TreeNavigator_DataBinding_Csharp/TreeNavigator_DataBinding/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TreeNavigator_DataBinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Syncfusion.Windows.Forms.Tools.TreeNavigator.HeaderCollection headerCollection2 = new Syncfusion.Windows.Forms.Tools.TreeNavigator.HeaderCollection();
+            this.treeNavigator1 = new Syncfusion.Windows.Forms.Tools.TreeNavigator();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeNavigator1
+            // 
+            headerCollection2.Font = new System.Drawing.Font("Arial", 8F);
+            headerCollection2.HeaderText = "TreeNavigator";
+            this.treeNavigator1.Header = headerCollection2;
+            this.treeNavigator1.ItemBackColor = System.Drawing.SystemColors.Control;
+            this.treeNavigator1.Location = new System.Drawing.Point(67, 75);
+            this.treeNavigator1.MinimumSize = new System.Drawing.Size(150, 150);
+            this.treeNavigator1.Name = "treeNavigator1";
+            this.treeNavigator1.Size = new System.Drawing.Size(313, 422);
+            this.treeNavigator1.TabIndex = 0;
+            this.treeNavigator1.Text = "treeNavigator1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(551, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load TreeNavigator";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 599);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeNavigator1);
+            this.Name = "Form1";
+            this.Text = "TreeNavigator_DataBinding";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.TreeNavigator treeNavigator1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TreeNavigator_DataBinding_VB/WF_TreeNavigatorDirectory/Form1.vb
@@ -0,0 +1,60 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Linq
+Imports System.Windows.Forms
+Imports Syncfusion.Windows.Forms.Tools
+Imports System.Data.SqlServerCe
+
+Namespace WF_TreeNavigatorDirectory
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+			dataBaseView = GetOrderDetials()
+		End Sub
+
+		Private dataBaseView As DataView
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Me.treeNavigator1.Visible = True
+			FillItems(Me.treeNavigator1)
+		End Sub
+		Private Sub FillItems(ByVal tree As TreeNavigator)
+		  For Each col As DataColumn In dataBaseView.Table.Columns
+				Me.treeNavigator1.Items.Add(New TreeMenuItem() With {.Text = col.ColumnName})
+		  Next col
+			For Each x As DataRow In dataBaseView.Table.Rows
+				For Each y As TreeMenuItem In Me.treeNavigator1.Items
+					If dataBaseView.Table.Columns.Contains(y.Text) Then
+						y.Items.Add(New TreeMenuItem() With {.Text = x(dataBaseView.Table.Columns(y.Text)).ToString()})
+					End If
+
+				Next y
+			Next x
+		End Sub
+
+		Private Shared Function GetOrderDetials() As DataView
+			Try
+				Dim ds As New DataSet()
+				Using con As New SqlCeConnection("DataSource=Data\Northwind.sdf")
+					con.Open()
+					Dim cmd As New SqlCeCommand("SELECT Top(20) [Order ID], [Product ID], [ColumnName], [Unit Price], Quantity" & ControlChars.CrLf & "                                                          FROM [Order Details] ", con)
+					Dim da As New SqlCeDataAdapter(cmd)
+
+
+					da.Fill(ds)
+
+					'Creation of DataView
+					Dim dataView As New DataView(ds.Tables(0), "[Unit Price] >= 20", "[Order ID]", DataViewRowState.CurrentRows)
+					Return dataView
+				End Using
+
+
+			Catch e As Exception
+				MessageBox.Show("Exception in connection: " & Constants.vbTab + e.Message)
+				Return Nothing
+			End Try
+		End Function
+	End Class
+End Namespace
