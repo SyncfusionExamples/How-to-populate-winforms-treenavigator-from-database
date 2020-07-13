1904a4649d831131db900df520973e44421cb2b4
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-07-13 11:30:23 +0530

    Modified the sample

--- TreeNavigator_DataBinding_Csharp/TreeNavigator_DataBinding/Form1.Designer.cs
+++ TreeNavigator_DataBinding_Csharp/TreeNavigator_DataBinding/Form1.Designer.cs
@@ -28,29 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Syncfusion.Windows.Forms.Tools.TreeNavigator.HeaderCollection headerCollection2 = new Syncfusion.Windows.Forms.Tools.TreeNavigator.HeaderCollection();
+            Syncfusion.Windows.Forms.Tools.TreeNavigator.HeaderCollection headerCollection1 = new Syncfusion.Windows.Forms.Tools.TreeNavigator.HeaderCollection();
             this.treeNavigator1 = new Syncfusion.Windows.Forms.Tools.TreeNavigator();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // treeNavigator1
             // 
-            headerCollection2.Font = new System.Drawing.Font("Arial", 8F);
-            headerCollection2.HeaderText = "TreeNavigator";
-            this.treeNavigator1.Header = headerCollection2;
+            this.treeNavigator1.Dock = System.Windows.Forms.DockStyle.Left;
+            headerCollection1.Font = new System.Drawing.Font("Arial", 8F);
+            headerCollection1.HeaderText = "TreeNavigator";
+            this.treeNavigator1.Header = headerCollection1;
             this.treeNavigator1.ItemBackColor = System.Drawing.SystemColors.Control;
-            this.treeNavigator1.Location = new System.Drawing.Point(67, 75);
-            this.treeNavigator1.MinimumSize = new System.Drawing.Size(150, 150);
+            this.treeNavigator1.Location = new System.Drawing.Point(0, 0);
+            this.treeNavigator1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.treeNavigator1.MinimumSize = new System.Drawing.Size(100, 97);
             this.treeNavigator1.Name = "treeNavigator1";
-            this.treeNavigator1.Size = new System.Drawing.Size(313, 422);
+            this.treeNavigator1.Size = new System.Drawing.Size(209, 306);
             this.treeNavigator1.TabIndex = 0;
             this.treeNavigator1.Text = "treeNavigator1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(551, 234);
+            this.button1.Location = new System.Drawing.Point(315, 130);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(240, 46);
+            this.button1.Size = new System.Drawing.Size(160, 30);
             this.button1.TabIndex = 1;
             this.button1.Text = "Load TreeNavigator";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,12 +61,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(971, 599);
+            this.ClientSize = new System.Drawing.Size(566, 306);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.treeNavigator1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TreeNavigator_DataBinding";
             this.ResumeLayout(false);
 
